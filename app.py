979d447b24b2aa8506714d69402d68338af08931
Marwan Mai <emef4z@gmail.com>
2024-03-02 12:17:40 +0100

    WIP - Populations page student table populating. Pass column not added

--- app.py
+++ app.py
@@ -15,8 +15,8 @@ def executeQuery(query):
     cursor.execute(query)
 
     result = cursor.fetchall()
-    cursor.close()
-    mydb.close()
+    # cursor.close()
+    # mydb.close()
     return result
 
 @app.route('/')
@@ -29,21 +29,32 @@ def index():
 
     return render_template('index.html',student_population=student_population)
 
-@app.route('/populations')
-def populations():
-    mycursor = mydb.cursor()
-    query_population = """
+
+@app.route('/populations/<int:year>/<batch>/<programme>')
+def populations(year, batch, programme):
+    student_population_query = f"""SELECT s.student_epita_email,c.contact_first_name ,c.contact_last_name
+FROM students s 
+JOIN contacts c
+ON s.student_contact_ref = c.contact_email WHERE s.student_population_code_ref = '{programme}' AND s.student_population_year_ref = {year} AND s.student_population_period_ref = '{batch}'"""
+
+    query_courses = """
     SELECT student_population_code_ref,student_population_period_ref,student_population_year_ref, count(*) AS population FROM students
     GROUP BY student_population_code_ref,student_population_period_ref,student_population_year_ref ORDER BY student_population_code_ref"""
 
-    mycursor.execute(query_population)
-    student_population = mycursor.fetchall()
-
-
-    # mydb.close()
+    student_population = executeQuery(student_population_query)
 
     return render_template('populations.html',student_population=student_population)
 
+
+@app.route('/populationss/<int:year>/<batch>/<programme>')
+def populationss(year, batch, programme):
+    test_query = f"SELECT * FROM students WHERE student_population_year_ref = {year} AND student_population_period_ref = '{batch}' AND student_population_code_ref = '{programme}'"
+    student_population_query = f"""SELECT s.student_epita_email,c.contact_first_name ,c.contact_last_name
+FROM students s 
+JOIN contacts c
+ON s.student_contact_ref = c.contact_email WHERE s.student_population_code_ref = '{programme}' AND s.student_population_year_ref = {year} AND s.student_population_period_ref = '{batch}'"""
+    student_population = executeQuery(student_population_query)
+    return render_template('populations.html',student_population=student_population)
 
 if __name__ == '__main__':
     app.run(debug=True)
